6fe836e7f3e39fe47675acd7e2432910f04d1c9e
LukeNK <lukenguyenkhanh@gmail.com>
2022-07-16 14:15:44 -0700

    Article numbering fix

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs')
 
 /**
  * @param {Array.<Object>} json Object to pass
+ * @param {Object.<Number>} art Article number object, pass as reference
  */
-function jsonToHTML(inp) {
+function jsonToHTML(inp, art) {
   let out = '';
+  if (art == undefined)
+    art = {n: 1}; // first recursive chain, pass as reference
   for (const obj of inp) {
     let pro, name, content = obj.content;
     // check for allowed section name
-    for (pro of ['title', 'chapter', 'part', 'article']) {
+    for (pro of ['title', 'subtitle', 'chapter', 'part', 'article', 'section']) {
       if (!obj[pro]) continue;
       name = obj[pro];
       break;
     }
+    if (typeof(content) == 'string' && pro == 'article') {
+      name = `Article ${art.n}. ${name}`;
+      art.n++;
+    }
     out += `<details class="${pro}"><summary>${name}</summary>`
-    if (typeof(content) == 'string')
-      out += `<p>${content}</p></details>`;
-    else {
+    if (typeof(content) == 'string') {
+      out += `<p>${content}</p>`;
+      if (obj.source) out += `<code>${obj.source}</code>`
+      out += '</details>';
+    } else {
       // object content, do recursive
-      out += `${jsonToHTML(obj.content)}</details>`
+      out += `${jsonToHTML(obj.content, art)}</details>`
     }
   }
   return out.replace(RegExp('\n', 'g'), '<br>')
@@ -27,7 +37,7 @@ function jsonToHTML(inp) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', data: jsonToHTML(require('../public/code.json')) });
+  res.render('index', { title: 'The Code of Laws of the Bayer Free State', data: jsonToHTML(JSON.parse(fs.readFileSync('./public/code.json', 'utf-8'))) });
 });
 
 module.exports = router;

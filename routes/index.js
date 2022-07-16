be3e6a7138713a0be67a69286cf2dbe16b7fd5e1
LukeNK <lukenguyenkhanh@gmail.com>
2022-07-16 13:15:19 -0700

    Basic navigation and `code.json`

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * @param {Array.<Object>} json Object to pass
+ */
+function jsonToHTML(inp) {
+  let out = '';
+  for (const obj of inp) {
+    let pro, name, content = obj.content;
+    // check for allowed section name
+    for (pro of ['title', 'chapter', 'part', 'article']) {
+      if (!obj[pro]) continue;
+      name = obj[pro];
+      break;
+    }
+    out += `<details class="${pro}"><summary>${name}</summary>`
+    if (typeof(content) == 'string')
+      out += `<p>${content}</p></details>`;
+    else {
+      // object content, do recursive
+      out += `${jsonToHTML(obj.content)}</details>`
+    }
+  }
+  return out.replace(RegExp('\n', 'g'), '<br>')
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', data: jsonToHTML(require('../public/code.json')) });
 });
 
 module.exports = router;
